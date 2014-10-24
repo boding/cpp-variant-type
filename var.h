bdadf47ab7a18d3f6a7215cfb6770e7e24a7d965
bobding <bobding@bobdings-MacBook-Pro.local>
2014-10-24 20:12:23 +0800

    add

---
+++ var.h
@@ -0,0 +1,404 @@
+/************************************************
+ *
+ * file  : var.h
+ * author: bobding
+ * date  : 2014-10-24
+ * detail:
+ *
+************************************************/
+
+#ifndef _VAR_H_
+#define _VAR_H_
+
+#include "cocos2d.h"
+#include <string>
+
+class var
+{
+public:
+    var()
+    {
+        clear();
+    }
+
+    ~var()
+    {
+        clear();
+    }
+
+    var(const var& rhs)
+    {
+        if (STRING == rhs.mType)
+        {
+            mValue.stringValue = new std::string(*rhs.mValue.stringValue);
+        }
+        else
+        {
+            mValue = rhs.mValue;
+        }
+
+        mType = rhs.mType;
+    }
+
+    var& operator = (const var& rhs)
+    {
+        if (STRING == rhs.mType)
+        {
+            mValue.stringValue = new std::string(*rhs.mValue.stringValue);
+        }
+        else
+        {
+            mValue = rhs.mValue;
+        }
+
+        mType = rhs.mType;
+
+        return *this;
+    }
+
+    /* boolean *********************************/
+    var(bool value)
+    {
+        clear();
+        mType = BOOL;
+        mValue.booleanValue = value;
+    }
+
+    var& operator = (bool value)
+    {
+        clear();
+        mType = BOOL;
+        mValue.booleanValue = value;
+        return *this;
+    }
+
+    bool asBool()
+    {
+        CCASSERT(BOOL == mType, "[var::asBool] got invalid type.");
+        return mValue.booleanValue;
+    }
+
+    /* signed char *****************************/
+    var(signed char value)
+    {
+        clear();
+        mType = INT8;
+        mValue.int8Value = value;
+    }
+
+    var& operator = (char value)
+    {
+        clear();
+        mType = INT8;
+        mValue.int8Value = value;
+        return *this;
+    }
+
+    char asChar()
+    {
+        CCASSERT(INT8 == mType, "[var::asChar] got invalid type.");
+        return mValue.int8Value;
+    }
+
+    /* unsigned char ***************************/
+    var(unsigned char value)
+    {
+        clear();
+        mType = INT8;
+        mValue.int8Value = (char)value;
+    }
+
+    var& operator = (unsigned char value)
+    {
+        clear();
+        mType = INT8;
+        mValue.int8Value = (char)value;
+        return *this;
+    }
+
+    unsigned char asUChar()
+    {
+        CCASSERT(INT8 == mType, "[var::asUChar] got invalid type.");
+        return (unsigned char)mValue.int8Value;
+    }
+
+    /* short ***********************************/
+    var(short value)
+    {
+        clear();
+        mType = INT16;
+        mValue.int16Value = value;
+    }
+
+    var& operator = (short value)
+    {
+        clear();
+        mType = INT16;
+        mValue.int16Value = value;
+        return *this;
+    }
+
+    short asShort()
+    {
+        CCASSERT(INT16 == mType, "[var::asShort] got invalid type.");
+        return mValue.int16Value;
+    }
+
+    /* unsigned short **************************/
+    var(unsigned short value)
+    {
+        clear();
+        mType = INT16;
+        mValue.int16Value = (short)value;
+    }
+
+    var& operator = (unsigned short value)
+    {
+        clear();
+        mType = INT16;
+        mValue.int16Value = (short)value;
+        return *this;
+    }
+
+    unsigned short asUShort()
+    {
+        CCASSERT(INT16 == mType, "[var::asUShort] got invalid type.");
+        return (unsigned short)mValue.int16Value;
+    }
+
+    /* int *************************************/
+    var(int value)
+    {
+        clear();
+        mType = INT32;
+        mValue.int32Value = value;
+    }
+
+    var& operator = (int value)
+    {
+        clear();
+        mType = INT32;
+        mValue.int32Value = value;
+        return *this;
+    }
+
+    int asInt()
+    {
+        CCASSERT(INT32 == mType, "[var::asInt] got invalid type.");
+        return mValue.int32Value;
+    }
+
+    /* unsigned int ****************************/
+    var(unsigned int value)
+    {
+        clear();
+        mType = INT32;
+        mValue.int32Value = (int)value;
+    }
+
+    var& operator = (unsigned int value)
+    {
+        clear();
+        mType = INT32;
+        mValue.int32Value = (int)value;
+        return *this;
+    }
+
+    unsigned int asUInt()
+    {
+        CCASSERT(INT32 == mType, "[var::asUInt] got invalid type.");
+        return (unsigned int)mValue.int32Value;
+    }
+
+    /* int64 ***********************************/
+    var(long long value)
+    {
+        clear();
+        mType = INT64;
+        mValue.int64Value = value;
+    }
+
+    var& operator = (long long value)
+    {
+        clear();
+        mType = INT64;
+        mValue.int64Value = value;
+        return *this;
+    }
+
+    long long asInt64()
+    {
+        CCASSERT(INT64 == mType, "[var::asInt64] got invalid type.");
+        return mValue.int64Value;
+    }
+
+    /* unsigned int64 **************************/
+    var(unsigned long long value)
+    {
+        clear();
+        mType = INT64;
+        mValue.int64Value = (long long)value;
+    }
+
+    var& operator = (unsigned long long value)
+    {
+        clear();
+        mType = INT64;
+        mValue.int64Value = (long long)value;
+        return *this;
+    }
+
+    unsigned long long asUInt64()
+    {
+        CCASSERT(INT64 == mType, "[var::asUInt64 got] invalid type.");
+        return (unsigned long long)mValue.int64Value;
+    }
+
+    /* pointer *********************************/
+    var(void* value)
+    {
+        clear();
+        mType = POINTER;
+        mValue.pointerValue = value;
+    }
+
+    var& operator = (void* value)
+    {
+        clear();
+        mType = POINTER;
+        mValue.pointerValue = value;
+        return *this;
+    }
+
+    void* asPointer()
+    {
+        CCASSERT(POINTER == mType, "[var::asPointer] got invalid type.");
+        return mValue.pointerValue;
+    }
+
+    /* float ***********************************/
+    var(float value)
+    {
+        clear();
+        mType = FLOAT;
+        mValue.floatValue = value;
+    }
+
+    var& operator = (float value)
+    {
+        clear();
+        mType = FLOAT;
+        mValue.floatValue = value;
+        return *this;
+    }
+
+    float asFloat()
+    {
+        CCASSERT(FLOAT == mType, "[var::asFloat] got invalid type.");
+        return mValue.floatValue;
+    }
+
+    /* double **********************************/
+    var(double value)
+    {
+        clear();
+        mType = DOUBLE;
+        mValue.doubleValue = value;
+    }
+
+    var& operator = (double value)
+    {
+        clear();
+        mType = DOUBLE;
+        mValue.doubleValue = value;
+        return *this;
+    }
+
+    double asDouble()
+    {
+        CCASSERT(DOUBLE == mType, "[var::asDouble] got invalid type.");
+        return mValue.doubleValue;
+    }
+
+    /* string **********************************/
+    var(const char* value)
+    {
+        clear();
+        mType = STRING;
+        mValue.stringValue = new std::string(value);
+    }
+
+    var(const std::string& value)
+    {
+        clear();
+        mType = STRING;
+        mValue.stringValue = new std::string(value);
+    }
+
+    var& operator = (const char* value)
+    {
+        clear();
+        mType = STRING;
+        mValue.stringValue = new std::string(value);
+        return *this;
+    }
+
+    var& operator = (const std::string& value)
+    {
+        clear();
+        mType = STRING;
+        mValue.stringValue = new std::string(value);
+        return *this;
+    }
+
+    std::string& asString()
+    {
+        CCASSERT(STRING == mType, "[var::asString] got invalid type.");
+        CCASSERT(0 != mValue.stringValue, "[var::asString] got invalid type.");
+
+        return *mValue.stringValue;
+    }
+
+protected:
+    enum Type
+    {
+        NONE, BOOL, INT8, INT16, INT32, INT64, POINTER, FLOAT, DOUBLE, STRING,
+    };
+
+    union Value
+    {
+        bool         booleanValue;
+        signed char  int8Value;
+        short        int16Value;
+        int          int32Value;
+        long long    int64Value;
+        void*        pointerValue;
+        float        floatValue;
+        double       doubleValue;
+        std::string* stringValue;
+
+        Value()
+        {
+            memset(this, 0, sizeof(Value));
+        }
+    };
+
+    void clear()
+    {
+        if (STRING == mType && 0 != mValue.stringValue)
+        {
+            mValue.stringValue->clear();
+            delete mValue.stringValue;
+        }
+
+        memset(&mValue, 0, sizeof(Value));
+
+        mType = NONE;
+    }
+
+protected:
+    Type mType;
+    Value mValue;
+};
+
+#endif // _VAR_H_
---
+++ varlist.h
@@ -0,0 +1,193 @@
+/************************************************
+ *
+ * file  : varlist.h
+ * author: bobding
+ * date  : 2014-10-24
+ * detail:
+ *
+************************************************/
+
+#ifndef _VARLIST_H_
+#define _VARLIST_H_
+
+#include "var.h"
+#include <vector>
+
+class varlist
+{
+public:
+    ~varlist()
+    {
+        mVars.clear();
+    }
+
+    varlist& operator << (bool value)
+    {
+        return addBool(value);
+    }
+
+    varlist& operator << (signed char value)
+    {
+        return addChar(value);
+    }
+
+    varlist& operator << (unsigned char value)
+    {
+        return addUChar(value);
+    }
+
+    varlist& operator << (short value)
+    {
+        return addShort(value);
+    }
+
+    varlist& operator << (unsigned short value)
+    {
+        return addUShort(value);
+    }
+
+    varlist& operator << (int value)
+    {
+        return addInt(value);
+    }
+
+    varlist& operator << (unsigned int value)
+    {
+        return addUInt(value);
+    }
+
+    varlist& operator << (long long value)
+    {
+        return addInt64(value);
+    }
+
+    varlist& operator << (unsigned long long value)
+    {
+        return addUInt64(value);
+    }
+
+    varlist& operator << (void* value)
+    {
+        return addPointer(value);
+    }
+
+    varlist& operator << (float value)
+    {
+        return addFloat(value);
+    }
+
+    varlist& operator << (double value)
+    {
+        return addDouble(value);
+    }
+
+    varlist& operator << (const char* value)
+    {
+        return addString(value);
+    }
+
+    varlist& operator << (const std::string& value)
+    {
+        return addString(value);
+    }
+
+    varlist& addBool(bool value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addChar(signed char value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addUChar(unsigned char value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addShort(short value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addUShort(unsigned short value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addInt(int value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addUInt(unsigned int value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addInt64(long long value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addUInt64(unsigned long long value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addPointer(void* value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addFloat(float value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addDouble(double value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addString(const char* value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    varlist& addString(const std::string& value)
+    {
+        mVars.push_back(var(value));
+        return *this;
+    }
+
+    unsigned int size()
+    {
+        return mVars.size();
+    }
+
+    var& at(int index)
+    {
+        CCASSERT(index >= 0 && index < (int)mVars.size(), "[varlist::at] invalid index.");
+        return mVars[index];
+    }
+
+protected:
+    std::vector<var> mVars;
+};
+
+#endif // _VARLIST_H_
